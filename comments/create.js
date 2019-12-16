ea7268b637a42d50b8c360f2db1ee8231f20e3d2
Hannah Stahl <hls62@cornell.edu>
2019-12-16 08:34:40 -0600

    Added DB table for blog post comments

---
+++ comments/create.js
@@ -0,0 +1,26 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.commentsTableName,
+    Key: {
+      blogPostId: event.pathParameters.id
+    },
+    Item: {
+      commentId: uuid.v1(),
+      name: data.name,
+      content: data.content,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
---
+++ comments/list.js
@@ -0,0 +1,18 @@
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.commentsTableName,
+    KeyConditionExpression: "blogPostId = :blogPostId",
+    ExpressionAttributeValues: {
+      ":blogPostId": event.pathParameters.id
+    }
+  };
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    return success(result.Items);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
